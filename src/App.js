5be31bf10dfa0935cb25dde497fdcae8d8f821fa
Yosh <antonyescor4@gmail.com>
2023-06-13 19:16:36 -0600

    Base Document

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import BasicExample from './components/alert.jsx';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Yosh Escorcia</h1>
+        <BasicExample></BasicExample>
       </header>
     </div>
   );
