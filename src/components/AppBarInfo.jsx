2e111241b94f6ca43f28d3cb2badd26a8bbbf6d0
Yosh <antonyescor4@gmail.com>
2023-06-16 14:18:09 -0600

    Style of subbarinfo complete with styles.

--- src/components/AppBarInfo.jsx
+++ src/components/AppBarInfo.jsx
@@ -2,33 +2,41 @@ import React from 'react';
 
 const BarInfo = () => {
     return (
-        <div class='searchbar right'>
-            <ul class='listIcons '>
-                <li class='social-icon AppBarInfo '>
-                    <a href=" ">NEWS + FEATURES</a>
-                </li>
-                <li class='social-icon AppBarInfo '>
-                    <a href=" ">VIDEO</a>
-                </li>
-                <li class='social-icon AppBarInfo '>
-                    <a href=" ">FILMS</a>
-                </li>
-                <li class='social-icon AppBarInfo '>
-                    <a href=" ">SERIES</a>
-                </li>
-                <li class='social-icon AppBarInfo '>
-                    <a href=" ">INTERACTIVE</a>
-                </li>
-                <li class='social-icon AppBarInfo '>
-                    <a href=" ">COMMUNITY</a>
-                </li>
-                <li class='social-icon AppBarInfo '>
-                    <a href=" ">DATABANK</a>
-                </li>
-                <li class='social-icon AppBarInfo '>
-                    <a href=" ">DISNEY+</a>
-                </li>
-            </ul>
+        <div>
+            <div class='AppBarInfoDiv right '>
+                <ul class='listIcons '>
+                    <li class='social-icon AppBarInfo '>
+                        <a href=" ">NEWS + FEATURES</a>
+                    </li>
+                    <li class='social-icon AppBarInfo '>
+                        <a href=" ">VIDEO</a>
+                    </li>
+                    <li class='social-icon AppBarInfo '>
+                        <a href=" ">FILMS</a>
+                    </li>
+                    <li class='social-icon AppBarInfo '>
+                        <a href=" ">SERIES</a>
+                    </li>
+                    <li class='social-icon AppBarInfo '>
+                        <a href=" ">INTERACTIVE</a>
+                    </li>
+                    <li class='social-icon AppBarInfo '>
+                        <a href=" ">COMMUNITY</a>
+                    </li>
+                    <li class='social-icon AppBarInfo '>
+                        <a href=" ">DATABANK</a>
+                    </li>
+                    <li class='social-icon AppBarInfo '>
+                        <a href=" ">DISNEY+</a>
+                    </li>
+                </ul>
+            </div>
+            <div class='SubBarInfor'>
+                <ul class='SubBarListLI'>
+                    <li>ALL OF YOUR STAR WARS FAVORITES NOW STREAMING ON DISNEY+</li>
+                </ul>
+
+            </div>
         </div>
     );
 };
