c45f3a895cb4b4f7c8dc8e721aa18a1ef41e3724
TONY MJ <56279166+tonymj23@users.noreply.github.com>
2023-06-16 00:29:25 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Yosh Escorcia</h1>
-        <BasicExample></BasicExample>
+    
       </header>
     </div>
   );
