b1d1f358042fcc0a9f4b7f9894e975c6d05e97a2
Tony MJ <tonymj1823@gmail.com>
2023-06-16 20:27:51 -0600

    fixing icons

--- src/components/socialIcons.jsx
+++ src/components/socialIcons.jsx
@@ -1,62 +1,66 @@
-import React from 'react';
+import React from "react";
+import { BsFacebook, BsInstagram, BsTwitter, BsYoutube } from "react-icons/bs";
 
 const IconsLeft = () => {
-    return (
-        <div class='social-links left '>
-            <ul class="listIcons ">
-                <li class="facebook social-icon ">
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
-                        </svg>
-                    </button>
-                </li>
-                <li class="facebook social-icon">
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
-                            <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-                            <path d="M16.5 7.5l0 .01" />
-                        </svg>
-                    </button>
-                </li>
-                <li class="facebook social-icon">
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-youtube" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M3 5m0 4a4 4 0 0 1 4 -4h10a4 4 0 0 1 4 4v6a4 4 0 0 1 -4 4h-10a4 4 0 0 1 -4 -4z" />
-                            <path d="M10 9l5 3l-5 3z" />
-                        </svg>
-                    </button>
-                </li>
-                <li class="facebook social-icon">
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-twitter" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c0 -.249 1.51 -2.772 1.818 -4.013z" />
-                        </svg>
-                    </button>
-                </li>
-                <li class="facebook social-icon">|</li>
-                <li class="facebook social-icon">
-                    <div class="d-flex justify-content-center align-items-center">
-                        <a
-                            class=" social-icon-kids"
-                            href="https://www.starwarskids.com/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-
-                        </a>
-                    </div>
-
-                </li>
-            </ul>
-
-        </div>
-    );
+  return (
+    <div class="social-links left ">
+      <ul class="listIcons ">
+        <li class="facebook social-icon ">
+          <a
+            class="sm-col"
+            href="https://www.facebook.com/starwarsla/?brand_redir=169299103121699"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsFacebook className="iconSize" />
+          </a>
+        </li>
+        <li class="facebook social-icon">
+          <a
+            class="sm-col"
+            href="https://www.instagram.com/starwars/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsInstagram className="iconSize" />
+          </a>
+        </li>
+        <li class="facebook social-icon">
+          <a
+            class="sm-col"
+            href="https://www.youtube.com/user/starwars"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsYoutube className="iconSize" />
+          </a>
+        </li>
+        <li class="facebook social-icon">
+          <a
+            class="sm-col"
+            href="https://twitter.com/starwars"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsTwitter className="iconSize" />
+          </a>
+        </li>
+        <li>
+          <span className="separator"></span>
+        </li>
+        <li class="facebook social-icon">
+          <div class="d-flex justify-content-center align-items-center">
+            <a
+              class="social-icon-kids"
+              href="https://www.starwarskids.com/ iconSize"
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default IconsLeft;
