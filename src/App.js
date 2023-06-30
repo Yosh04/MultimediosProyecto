1dab7ba2a2784b1ce5321592d12afe3851806e49
Peter Sequeira <pitsix23@gmail.com>
2023-06-29 23:08:06 -0600

    update carousel

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import React from 'react';
 import './App.css';
-
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import LogoStarWars from './components/logoStartWars';
 import IconsLeft from './components/socialIcons';
 import ResearchIcon from './components/searchIcons';
 import BarInfo from './components/AppBarInfo';
 import CarouselComponent from './components/CarouselComponent';
 import Section2clips from './components/Secction2clips';
-
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
 
 
 function App() {
+  const images = [
+    { src: 'https://images7.alphacoders.com/671/thumbbig-671281.webp', alt: 'Image 1' },
+    { src: 'https://images3.alphacoders.com/114/thumbbig-11439.webp', alt: 'Image 2' },
+    { src: 'https://images8.alphacoders.com/131/thumbbig-1311649.webp', alt: 'Image 3' },
+    { src: 'https://images3.alphacoders.com/131/thumbbig-1310689.webp', alt: 'Image 4' },
+  
+  ];
   return (
     <div className="App">
       <header className="App-header ">
@@ -28,7 +32,7 @@ function App() {
         
         
       </header>
-      <CarouselComponent />
+      <CarouselComponent images={images} />
       <Section2clips></Section2clips>
 
       <Footer />  
