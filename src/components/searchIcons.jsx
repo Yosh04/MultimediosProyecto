36acddcf2310c49592075610c07af75bde85c830
Yosh <antonyescor4@gmail.com>
2023-07-01 18:14:02 -0600

    searh icon complete with its funcionalibility, working on sign up

--- src/components/searchIcons.jsx
+++ src/components/searchIcons.jsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Overlay from 'react-bootstrap/Overlay';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+
+import React, { useState, useRef } from 'react';
 
 import { GiCancel } from "react-icons/gi";
 import { BsSearch } from "react-icons/bs";
 
 
-
 const ResearchIcon = () => {
+    //Codigo para el overlay,
+    const [show, setShow] = useState(false);
+    const target = useRef(null);
 
     const [showInput, setShowInput] = useState(false);
 
@@ -19,11 +26,11 @@ const ResearchIcon = () => {
 
     return (
         <div class='searchbar'>
-            <ul class='listIconsSocial '>
-                <li class='social-icon  '>
+            <ul class=' listIconsHorizontal searchMargin '>
+                <li class='social-icon'>
                     {!showInput ? (
                         <div className="inputContainer">
-                            <button class='listIcons ' onClick={handleButtonClick}>
+                            <button onClick={handleButtonClick}>
                                 <svg xmlns="http://www.w3.org/2000/svg" class="" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                     <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
@@ -33,25 +40,52 @@ const ResearchIcon = () => {
                             </button>
                         </div>
                     ) : (
-                        <div className="inputContainer">
+                        <div className="inputContainer ">
                             <button onClick=''><BsSearch className="IconSearch" /></button>
-                            <input type="text" />
+                            <input placeholder="Search" type="text" />
                             <button onClick={handleCancelButtonClick}><GiCancel className="IconCancel" /></button>
                         </div>
                     )}
                 </li>
-                <li class='social-icon searsicon'>
-                
-                    <button class='listIcons '>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
-                            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                        </svg>
-                        <p class='search-title'>SIGN IN</p>
-                    </button>
-                    
+                <li lass='social-icon'>
+                    <div>
+                        <Button variant="danger" ref={target} onClick={() => setShow(!show)} style={{ paddingTop: '25px', display: 'flex', background: 'transparent', border: 'none' }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                            </svg>
+                            <p class='search-title'>SIGN IN</p>
+                        </Button>
+                        <Overlay target={target.current} show={show} placement="left">
+                            {({ placement, arrowProps, show: _show, popper, ...props }) => (
+                                <div
+                                    {...props}
+                                    style={{
+                                        position: 'absolute',
+                                        padding: '10px 0px',
+                                        justifyItems: 'center',
+                                        borderRadius: 3,
+                                        ...props.style,
+                                    }}
+                                >
+                                    <Card style={{ width: '18rem' }}>
+                                        <Card.Img variant="top" src="holder.js/100px180" />
+                                        <Card.Body>
+                                            <Card.Title>Card Title</Card.Title>
+                                            <Card.Text>
+                                                Some quick example text to build on the card title and make up the
+                                                bulk of the card's content.
+                                            </Card.Text>
+                                            <Button variant="primary">Go somewhere</Button>
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                            )}
+                        </Overlay>
+                    </div>
                 </li>
+
             </ul>
         </div>
     );
